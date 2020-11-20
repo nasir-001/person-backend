83fb5f7602cbf06ff8dafa5126031bc8f71fa0a4
Nasir Lawal <nasirlawal001@gmail.com>
2020-11-20 10:31:39 +0100

    Change the port number for the request files

--- index.js
+++ index.js
@@ -110,6 +110,6 @@ app.post('/api/persons', (request, response) => {
 
 })
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT || 3002
 app.listen(PORT)
 console.log(`Server is running on port ${PORT}`);
